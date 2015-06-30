ecfbddc05338548972d9b2913b74f790e325ba13
Trey Hunner <trey@treyhunner.com>
2015-06-30 10:42:12 -0700

    Throttle function calls to avoid slowing page load

--- pridify.js
+++ pridify.js
@@ -3,6 +3,28 @@ var selectors = [
   'img[src*=".twimg.com/profile_images/"]',
 ];
 
+// Based on https://remysharp.com/2010/07/21/throttling-function-calls
+function throttle(func, threshhold) {
+  var before;
+  var timeout;
+  return function () {
+    var context = this;
+    var now = +new Date();
+    var args = arguments;
+    if (before && now < before + threshhold) {
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        before = now;
+        func.apply(context, args);
+      }, threshhold);
+    } else {
+      // don't delay, act now
+      before = now;
+      func.apply(context, args);
+    }
+  };
+}
+
 function pridifyImage(image) {
   var prideGradients = 'linear-gradient(to bottom, rgba(255, 62, 24, 0.5), rgba(255, 62, 24, 0.5) 16.666%, rgba(252, 154, 0, 0.5) 16.666%, rgba(252, 154, 0, 0.5) 33.333%, rgba(255, 216, 0, 0.5) 33.333%, rgba(255, 216, 0, 0.5) 50%, rgba(57, 234, 124, 0.5) 50%, rgba(57, 234, 124, 0.5) 66.666%, rgba(11, 178, 255, 0.5) 66.666%, rgba(11, 178, 255, 0.5) 83.333%, rgba(152, 90, 255, 0.5) 83.333%, rgba(152, 90, 255, 0.5))';
   image.style.backgroundImage = prideGradients + ', url(URL)'.replace(/URL/, image.src);
@@ -10,16 +32,16 @@ function pridifyImage(image) {
   image.src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
 }
 
-function pridifyAll(selector) {
+var pridifyAll = throttle(function (selector) {
   var images = document.querySelectorAll(selector);
   for (var i = 0; i < images.length; i++) {
     pridifyImage(images[i]);
   }
-}
+}, 500);
 
 var observer = new MutationObserver(function(mutations) {
   pridifyAll(selectors.join());
 });
 
-observer.observe(document.body, { subtree: true, childList: true, characterData: true });
+observer.observe(document.body, { subtree: true, childList: true });
 
